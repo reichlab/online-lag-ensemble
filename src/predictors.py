d77c6555f4de02634097323c2ef1d38f1546157d
Abhinav Tushar <lepisma@fastmail.com>
2018-04-22 17:41:47 -0400

    Use init_weights from the update_fn directly

--- src/predictors.py
+++ src/predictors.py
@@ -8,14 +8,17 @@ from ledge.datatypes import Prediction, Truth
 from utils.dists import weighted_prediction
 from functools import partial
 from typing import List
+from inspect import signature
 
 
 def _predictor(truths: List[Truth],
                preds: List[Prediction],
-               loss_fn, merging_fn, update_fn,
-               init_weights=None):
+               loss_fn, merging_fn, update_fn):
     output = []
     epiweeks = preds[0].coords["epiweek"]
+
+    # Extract init_weights from update_fn
+    init_weights = signature(update_fn).parameters["init_weights"].default
 
     for idx, ew in enumerate(epiweeks):
         if idx == 0:
@@ -23,7 +26,7 @@ def _predictor(truths: List[Truth],
         else:
             truth = merging_fn(mask_truths(truths, ew))
             losses = [loss_fn(pred.loc[:(ew - 1)], truth) for pred in preds]
-            weights = update_fn(losses, init_weights=init_weights)
+            weights = update_fn(losses)
             weights = weights / weights.sum()
 
         output.append(weighted_prediction([pred.loc[ew] for pred in preds], weights))
