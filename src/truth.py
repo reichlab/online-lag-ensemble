c12262efd10b5471a136a0b801a624f46d969b29
Abhinav Tushar <lepisma@fastmail.com>
2018-04-28 19:20:41 -0400

    Copy imputation builder inside ./src

--- src/truth.py
+++ src/truth.py
@@ -1,7 +1,9 @@
 from ledge.datatypes import Truth
 from ledge.utils import get_lag
+import ledge.fill as fill
 from pymmwr import Epiweek
-from typing import List
+from typing import List, Dict
+from functools import partial
 
 
 def mask_truths(truths: List[Truth], ew: Epiweek) -> List[Truth]:
@@ -17,3 +19,35 @@ def mask_truths(truths: List[Truth], ew: Epiweek) -> List[Truth]:
             masked.append(clipped)
 
     return masked
+
+
+def impute(truths: List[Truth], config: Dict) -> List[Truth]:
+    """
+    Impute the list of truths according to the config
+    """
+
+    if (len(truths) < 2) or (config["type"] is None):
+        return truths
+    elif config["type"] is "diff":
+        window = config["window"]
+        if window["type"] == "uniform":
+            window_fn = fill.window_uniform
+        elif window["type"] == "linear":
+            window_fn = partial(fill.window_linear, alpha=window["alpha"])
+        elif window["type"] == "geometric":
+            window_fn = partial(fill.window_geometric, gamma=window["gamma"])
+        else:
+            raise Exception("Window type not understood")
+
+        window_fn = fill.lookback(config["lookback"])(window_fn)
+        if config["normalize"]:
+            window_fn = fill.normalize(window_fn)
+
+        truths[:config["n_series"]] = fill.diff_window(
+            truths[:config["n_series"]],
+            window_fn,
+            inc=config["incremental"]
+        )
+        return truths
+    else:
+        raise Exception("Imputation type not understood")
