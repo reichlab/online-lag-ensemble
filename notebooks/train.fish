bcdd1ecdb3b4b5cb9bc359c6f1bb7efc0613ba26
Abhinav Tushar <lepisma@fastmail.com>
2018-05-03 00:40:44 -0400

    train fish script

---
+++ notebooks/train.fish
@@ -0,0 +1,30 @@
+# Notebook runner
+# Use source to run
+
+function run_notebook
+  set -l script (echo $argv[1] | sed "s/\.ipynb/.py/")
+
+  rm -f $script
+  jupyter nbconvert --to script $argv[1]
+
+  sed -i "/get_ipython()/d" $script
+
+  # Assuming region is set to nat
+  sed -i "s/REGION = \"nat\"/REGION = \"$argv[2]\"/" $script
+
+  # Assuming target is set to initial value
+  sed -i "s/TARGET = \"1-ahead\"/TARGET = \"$argv[3]\"/" $script
+
+  # Run stuff
+  python $script
+
+  # Start new
+  rm -f $script
+end
+
+for r in "nat" "hhs1" "hhs2" "hhs3" "hhs4" "hhs5" "hhs6" "hhs7" "hhs8" "hhs9" "hhs10"
+    for t in "1-ahead" "2-ahead" "3-ahead" "4-ahead"
+        echo "Running for target $t and region $r"
+        run_notebook ./04-train-multibin.ipynb $r $t
+    end
+end
